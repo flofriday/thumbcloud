f2f3aec9ecf3106e1e8c3c23ed6a108b6a27a1f3
flofriday <flohacksfriday@gmail.com>
2018-06-01 22:37:48 +0100

    Added websockets

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+extern crate actix;
 extern crate actix_web;
-use std::path::PathBuf;
+
+use actix::*;
+use actix_web::fs::NamedFile;
 use actix_web::*;
-use actix_web::{fs::NamedFile};
+use std::path::PathBuf;
 
 fn index(_req: HttpRequest) -> Result<NamedFile> {
     println!("fn1");
@@ -21,23 +24,36 @@ fn system(_req: HttpRequest) -> Result<NamedFile> {
     Ok(NamedFile::open(path)?)
 }
 
+struct Ws;
+
+impl Actor for Ws {
+    type Context = ws::WebsocketContext<Self>;
+}
+
+impl StreamHandler<ws::Message, ws::ProtocolError> for Ws {
+    fn handle(&mut self, msg: ws::Message, ctx: &mut Self::Context) {
+        match msg {
+            ws::Message::Ping(msg) => ctx.pong(&msg),
+            ws::Message::Text(text) => ctx.text(text),
+            ws::Message::Binary(bin) => ctx.binary(bin),
+            _ => (),
+        }
+    }
+}
+
 fn main() {
     server::new(|| {
         vec![
-            App::new().
-                prefix("/about")
-                .resource("/", |r| r.f(about)),
-            App::new().
-                prefix("/system")
-                .resource("/", |r| r.f(system)),
+            App::new().prefix("/about").resource("/", |r| r.f(about)),
+            App::new().prefix("/system").resource("/", |r| r.f(system)),
             App::new()
+                .resource("/ws/", |r| r.f(|req| ws::start(req, Ws)))
                 .handler(
                     "/",
-                    fs::StaticFiles::new("./static/")
-                    .default_handler(index)
-                    ),
+                    fs::StaticFiles::new("./static/").default_handler(index),
+                ),
         ]
     }).bind("127.0.0.1:80")
-    .expect("Can not start server on given IP/Port")
+        .expect("Can not start server on given IP/Port")
         .run();
 }
