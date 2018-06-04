a46d227186a22c4b3bdebd4fd2d85cf1c1c81eed
flofriday <flohacksfriday@gmail.com>
2018-06-05 00:19:06 +0100

    Added dependencies and modules

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
 extern crate actix;
 extern crate actix_web;
 
+extern crate serde;
+
+#[macro_use]
+extern crate serde_json;
+
+#[macro_use]
+extern crate serde_derive;
+
+pub mod args;
 pub mod webserver;
+pub mod decoder;
+pub mod files;
 
 fn main() {
     webserver::run();
 }
+
