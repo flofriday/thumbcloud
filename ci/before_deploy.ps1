1ff150b52cbb17a3898d5a3e9cf44a7b56422db7
flofriday <flohacksfriday@gmail.com>
2018-06-10 18:19:09 +0100

    Fixed adding static files to release

--- ci/before_deploy.ps1
+++ ci/before_deploy.ps1
@@ -14,7 +14,7 @@ $ZIP = "$SRC_DIR\$($Env:CRATE_NAME)-$($Env:APPVEYOR_REPO_TAG_NAME)-$($Env:TARGET
 Copy-Item "$SRC_DIR\target\$($Env:TARGET)\release\thumbcloud.exe" '.\'
 
 # Thumbcloud add static files to deploy folder
-Copy-Item "$SRC_DIR\static" '.\'
+Copy-Item "$SRC_DIR\static" '.\' -Recurse
 
 
 7z a "$ZIP" *
