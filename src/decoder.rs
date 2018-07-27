6e61171da2c38032f3e27d729a5b6dd06ed5d383
flofriday <flohacksfriday@gmail.com>
2018-07-27 19:43:08 +0200

    Fixed unintended console output

--- src/decoder.rs
+++ src/decoder.rs
@@ -19,7 +19,6 @@ pub fn decode(input: String, path_base: &PathBuf) -> String {
         let mut path: PathBuf = path_base.to_owned();
         path.push(&path_end);
 
-        println!("Open path: {:?}", path_end); //TODO: remove later
         return files::get_file_respond(path, path_end);
     } else {
         return json!({
